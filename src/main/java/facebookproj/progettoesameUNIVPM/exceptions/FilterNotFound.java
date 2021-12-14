f7eeee710a0c013f08e3e0d665f8ddb84cc79e94
Francesco Parisi <francescoparisi178@gmail.com>
2021-12-14 16:29:33 +0100

    modified Exceptions

--- src/main/java/facebookproj/progettoesameUNIVPM/exceptions/FilterNotFound.java
+++ src/main/java/facebookproj/progettoesameUNIVPM/exceptions/FilterNotFound.java
@@ -1,5 +1,15 @@
+/*
+ * @author Francesco R. Parisi
+ * @author Alessandro Rossini
+ * 
+ */
 package facebookproj.progettoesameUNIVPM.exceptions;
+import java.io.IOException;
 
-public class FilterNotFound {
-
+public class FilterNotFound extends IOException {
+	private static final long serialVersionUID = 1; // exception implements serializable interface
+	public FilterNotFound() {
+		super();
+		System.out.println("The following filter is not available");
+	}
 }
--- src/main/java/facebookproj/progettoesameUNIVPM/exceptions/ResponseNotFound.java
+++ src/main/java/facebookproj/progettoesameUNIVPM/exceptions/ResponseNotFound.java
@@ -1,5 +1,15 @@
+/*
+ * @author Francesco R. Parisi
+ * @author Alessandro Rossini
+ * 
+ */
 package facebookproj.progettoesameUNIVPM.exceptions;
-
-public class ResponseNotFound {
+import java.io.IOException;
+public class ResponseNotFound extends IOException {
+	private static final long serialVersionUID = 1; // exception implements serializable interface
+	public ResponseNotFound() {
+		super();
+		System.out.println("The JSON response is not found");
+	}
 
 }
